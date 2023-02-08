d9f65931200da3d7da4dad03fbb6fa1f524d7fc1
Ratan Masanta <ratanmasanta@gmail.com>
2023-02-08 09:45:49 +0530

    First file added

---
+++ src/com/masanta/ratan/daily/practice/design/algorithms/recursion/ReverseString.java
@@ -0,0 +1,15 @@
+package com.masanta.ratan.daily.practice.design.algorithms.recursion;
+
+public class ReverseString {
+    
+    public static void main(String[] args){
+        String input = "Collection";
+        System.out.println(reverse(input));
+    }
+
+    public static String reverse(String str)
+    {
+        if(str.isEmpty()) return str;
+        return str.substring(1) + str.charAt(0);
+    }
+}
