b77307e5bc5ac6569a1314cc77dc26ec49bc64ca
Ratan Masanta <ratanmasanta@gmail.com>
2023-03-29 21:50:11 +0530

    Changes on 29th March: LeetCOde: 1402. Reducing Dishes

--- src/com/masanta/ratan/daily/practice/leetcode/medium/MinimumPathSum.java
+++ src/com/masanta/ratan/daily/practice/leetcode/medium/MinimumPathSum.java
@@ -1,5 +1,7 @@
 package com.masanta.ratan.daily.practice.leetcode.medium;
 
+import java.util.Arrays;
+
 public class MinimumPathSum {
 
     /**
@@ -30,10 +32,84 @@ public class MinimumPathSum {
         return grid[m-1][n-1];
     }
 
+    public int minSumPathUtil(int i, int j, int[][] matrix, int[][] dp) {
+        if(i==0 && j == 0)
+            return matrix[0][0];
+        if(i<0 || j<0)
+            return (int)Math.pow(10,9);
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int up = matrix[i][j]+minSumPathUtil(i-1,j,matrix,dp);
+        int left = matrix[i][j]+minSumPathUtil(i,j-1,matrix,dp);
+
+        return dp[i][j] = Math.min(up,left);
+
+    }
+
+    public int minSumPath(int n, int m, int[][] matrix){
+
+        int dp[][]=new int[n][m];
+
+        for(int i=0; i<n ; i++){
+            for(int j=0; j<m; j++){
+                if(i==0 && j==0) dp[i][j] = matrix[i][j];
+                else{
+
+                    int up = matrix[i][j];
+                    if(i>0) up += dp[i-1][j];
+                    else up += (int)Math.pow(10,9);
+
+                    int left = matrix[i][j];
+                    if(j>0) left+=dp[i][j-1];
+                    else left += (int)Math.pow(10,9);
+
+                    dp[i][j] = Math.min(up,left);
+                }
+            }
+        }
+
+        return dp[n-1][m-1];
+
+    }
+
+
+    public int minSumPathSpaceOptimized(int n, int m, int[][] matrix){
+
+        int prev[] = new int[m];
+        for(int i=0; i<n ; i++){
+            int temp[] = new int[m];
+            for(int j=0; j<m; j++){
+                if(i==0 && j==0) temp[j] = matrix[i][j];
+                else{
+
+                    int up = matrix[i][j];
+                    if(i>0) up += prev[j];
+                    else up += (int)Math.pow(10,9);
+
+                    int left = matrix[i][j];
+                    if(j>0) left+=temp[j-1];
+                    else left += (int)Math.pow(10,9);
+
+                    temp[j] = Math.min(up,left);
+                }
+            }
+            prev=temp;
+        }
+
+        return prev[m-1];
+    }
+
     public static void main(String[] args) {
         int[][] grid = {{1,3,1},{1,5,1},{4,2,1}};
         MinimumPathSum minimumPathSum = new MinimumPathSum();
         System.out.println(minimumPathSum.minPathSum(grid));
+        int n = grid.length, m=grid[0].length;
+        int dp[][]=new int[n][m];
+        for(int row[]: dp)
+            Arrays.fill(row,-1);
+        System.out.println(minimumPathSum.minSumPathUtil(n -1, m - 1, grid, dp)) ;
+        System.out.println(minimumPathSum.minSumPath(n, m, grid));
+        System.out.println(minimumPathSum.minSumPathSpaceOptimized(n,m,grid));
     }
 
     /*
