ea154ac6face6c147ee92182780efef65cca6e5b
Ratan Masanta <ratanmasanta@gmail.com>
2023-05-27 23:34:38 +0530

    Added another approach in the HouseRobber problem

--- src/com/masanta/ratan/daily/practice/design/algorithms/dynamic_programming/HouseRobber.java
+++ src/com/masanta/ratan/daily/practice/design/algorithms/dynamic_programming/HouseRobber.java
@@ -1,5 +1,7 @@
 package com.masanta.ratan.daily.practice.design.algorithms.dynamic_programming;
 
+import java.util.Arrays;
+
 public class HouseRobber {
 
     /**
@@ -38,9 +40,71 @@ public class HouseRobber {
         return Math.max(rob, notrob);
     }
 
+    // recursion
+
+    /**
+     *
+     * @param nums number array
+     * @param i start index
+     * @param dp dp array
+     * @return max amount that can be robbed
+     */
+    private int solveMemo(int nums[],int i,int dp[]){
+
+        if(i>=nums.length) return 0;
+
+        if(dp[i]!=-1) return dp[i];
+
+        int a=nums[i]+solveMemo(nums,i+2,dp);
+        int b=solveMemo(nums,i+1,dp);
+
+        return dp[i]=Math.max(a,b);
+
+
+    }
+
+    // tabular
+    private int solveTab(int nums[]){
+
+        int len=nums.length;
+        int dp[]=new int[len+2];
+
+        for(int i=len-1;i>=0;i--){
+            int a=nums[i]+dp[i+2];
+            int b=dp[i+1];
+            dp[i]=Math.max(a,b);
+        }
+        return dp[0];
+    }
+
+    // SC = (1)
+    private int solve1(int nums[]){
+
+        int len=nums.length;
+
+        int prev=0;
+        int curr=0;
+
+        for(int i=len-1;i>=0;i--){
+
+            int a=nums[i]+prev;
+            int b=curr;
+            prev=curr;
+            curr=Math.max(a,b);
+
+        }
+
+        return curr;
+    }
+
     public static void main(String[] args) {
         HouseRobber houseRobber = new HouseRobber();
         int[] nums = {2,7,9,3,1};
         System.out.println(houseRobber.rob(nums));
+        System.out.println(houseRobber.solve1(nums));
+        System.out.println(houseRobber.solveTab(nums));
+        int[] dp = new int[nums.length + 1];
+        Arrays.fill(dp, -1);
+        System.out.println(houseRobber.solveMemo(nums, 0, dp));
     }
 }
