49d801da25ea18cbc33309324fd5ab43b984c164
Ratan Masanta <ratanmasanta@gmail.com>
2023-07-16 23:10:55 +0530

    Leetcode 14th July daily challenge refactor

--- src/com/masanta/ratan/daily/practice/leetcode/hard/MinimumInsertionStepsToMakeAStringPalindrome.java
+++ src/com/masanta/ratan/daily/practice/leetcode/hard/MinimumInsertionStepsToMakeAStringPalindrome.java
@@ -1,5 +1,7 @@
 package com.masanta.ratan.daily.practice.leetcode.hard;
 
+import java.util.Arrays;
+
 public class MinimumInsertionStepsToMakeAStringPalindrome {
 
     /**
@@ -103,6 +105,202 @@ public class MinimumInsertionStepsToMakeAStringPalindrome {
         System.out.println(minimumInsertionStepsToMakeAStringPalindrome.minInsertions(s1));
     }
 
+    public static class MaximumNumberOfEventsThatCanBeAttendedTwo {
+
+        /*
+            1751. Maximum Number of Events That Can Be Attended II
+            Hard
+            1.8K
+            33
+            Companies
+            You are given an array of events where events[i] = [startDayi, endDayi, valuei]. The ith event starts at startDayi and ends at endDayi, and if you attend this event, you will receive a value of valuei. You are also given an integer k which represents the maximum number of events you can attend.
+
+            You can only attend one event at a time. If you choose to attend an event, you must attend the entire event. Note that the end day is inclusive: that is, you cannot attend two events where one of them starts and the other ends on the same day.
+
+            Return the maximum sum of values that you can receive by attending events.
+
+
+
+            Example 1:
+
+
+
+            Input: events = [[1,2,4],[3,4,3],[2,3,1]], k = 2
+            Output: 7
+            Explanation: Choose the green events, 0 and 1 (0-indexed) for a total value of 4 + 3 = 7.
+         */
+
+        /**
+         *
+         * Overview
+         * We can only attend an event if the start day of it is greater than the end day of the previously attended event. This implies that we should sort events by their start time. As shown in the following figure, we sort events = [[1,2,4],[3,4,3],[2,3,1],[4,6,5],[2,4,8]] according to the start time of each event.
+         *
+         * img
+         *
+         * All subsequent solutions are based on the sorted events.
+         *
+         * Approach 1: Top-down Dynamic Programming + Binary Search
+         * Intuition
+         * If you are not familiar with dynamic programming, please refer to our explore cards Dynamic Programming Explore Card. We will focus on the usage in this article and not the underlying principles or implementation details.
+         *
+         * Let dfs(cur_index) represent the maximum value obtained by attending events optimally in the range events[cur_index ~ n - 1]
+         *
+         * For event cur_index, we have two options:
+         *
+         * attend the current event and gain a value of events[cur_index][2]. Then we need to find the nearest event that we can attend after event cur_index. Recall that we have sorted events by start time. We can apply binary search to find the index where we should insert the end time of the current event cur_index in the sorted list of start times. Let's say the nearest one is event next_index. Thus dfs(cur_index) is the larger value between the two options:
+         *
+         * attend the current event and obtain a value of events[cur_index][2] + dfs(next_index).
+         *
+         * skip the current event, move on to the next event, and gain a value of dfs(cur_index + 1).
+         *
+         * which is denoted as dfs(cur_index) = max(dfs(cur_index + 1), dfs(next_index) + events[cur_index][2]).
+         *
+         *
+         * As shown in the picture below, we find the insertion index is 3, which indicates that the nearest available event after event 0 is event 3.
+         *
+         * img
+         *
+         * Therefore, we can update dfs(0) as the larger value obtained by attending or skipping event 0.
+         *
+         * attend event 0 and get a value of events[0][2] + dfs(3).
+         * skip event 0 and get a value of dfs(1).
+         * img
+         *
+         * Given the restriction that we can attend a maximum of k events, we also need to keep track of count, the number of events we have attended so far. Therefore, we will redefine this function as dfs(cur_index, count).
+         *
+         * Additionally, we use memoization to store the maximum value obtained by each state (cur_index, count). This helps us avoid re-solving the same subproblems multiple times and significantly reduces the time complexity of the algorithm.
+         *
+         *
+         * Algorithm
+         * Sort events by start time.
+         *
+         * Build a 2D array dp of size (k+1)×n(k + 1) \times n(k+1)×n as memory.
+         *
+         * Define dfs(cur_index, count) as the maximum value obtained by attending a maximum of count events in the range events[cur_index ~ n - 1].
+         *
+         * If (count, cur_index) is already stored in dp, return dp[count][cur_index].
+         *
+         * Return 0 if count = 0 or cur_index = n.
+         *
+         * Skip this event and get the value of dfs(cur_index + 1, count).
+         *
+         * Find the index of the nearest available event next_index after the current event cur_index with binary search.
+         *
+         * Attend this event and get the value of dfs(next_index, count - 1) plus the value of this event events[cur_index][2].
+         *
+         * Store the larger one of the two values above in dp[count][cur_index] and return dp[count][cur_index].
+         *
+         * Return dfs(0, k).
+         *
+         * @param events int array with event details start ady, end day and value of the event
+         * @param k maximum number of events you can attend
+         * @return maximum sum of values that you can receive by attending events
+         */
+        public int maxValuePartOne(int[][] events, int k) {
+            Arrays.sort(events, (a, b) -> a[0] - b[0]);
+            int n = events.length;
+
+            dp = new int[k + 1][n];
+            for (int[] row : dp) {
+                Arrays.fill(row, -1);
+            }
+
+            return dfs(0, k, events);
+        }
+
+        private int[][] dp;
+        private int dfs(int curIndex, int count, int[][] events) {
+            if (count == 0 || curIndex == events.length) {
+                return 0;
+            }
+            if (dp[count][curIndex] != -1) {
+                return dp[count][curIndex];
+            }
+            int nextIndex = bisectRight(events, events[curIndex][1]);
+            dp[count][curIndex] = Math.max(dfs(curIndex + 1, count, events), events[curIndex][2] + dfs(nextIndex, count - 1, events));
+            return dp[count][curIndex];
+        }
+
+        private int bisectRight(int[][] events, int target) {
+            int left = 0, right = events.length;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (events[mid][0] <= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+
+
+        /**
+         * Approach 2: Bottom-up Dynamic Programming + Binary Search
+         * Intuition
+         * In the previous approach, we start with the original problem dfs(0, k) and recursively break it down into smaller subproblems. We can also use bottom-up DP that starts with the smallest subproblems and works its way up to the original problem.
+         *
+         * We can build a 2D array dp and let dp[count][cur_index] represent the maximum value we obtain by attending at most count events in the range events[cur_index ~ n - 1] (equivalent to dfs(cur_index, count) in the previous approach). We first solve the smallest subproblems, then use their solutions to solve slightly larger subproblems, and so on until we solve the original problem dp[0][k].
+         *
+         * For the current state dp[count][cur_index], we have two options:
+         *
+         * attend event cur_index and gain a value of events[cur_index][2]. Then we need to find the nearest events that we can attend after this event. Recall that we have sorted events according to the start times, so we can apply a binary search to find next_index, the inserting index of events[cur_index][1], the end time of this event, on the sorted start times. Thus the value we obtain is events[cur_index][2] + dp[count - 1][next_index].
+         *
+         * skip the event cur_index and move on to the next event, thus the value is equal to dp[count][cur_index + 1].
+         *
+         * Therefore, we have the recurrence relation as dp[count][cur_index] = max(dp[count][cur_index + 1], dp[count - 1][next_index] + events[cur_index][2]).
+         *
+         *
+         * Algorithm
+         * Sort events by start time.
+         *
+         * Define a dynamic programming table dp of size (k+1)⋅(n+1)(k + 1) \cdot (n + 1)(k+1)⋅(n+1).
+         *
+         * Iterate starting from the base cases. Iterate over events backward from n - 1 to 0. For each event, iterate over the number of events that can be attended from 1 to k.
+         *
+         * Locate nextIndex, the index of the first event whose starting time is greater than the end time of the current event curIndex using binary search.
+         *
+         * Update dp[count][curIndex] as max(dp[count][curIndex + 1], dp[count + 1][nextIndex] + events[curIndex][2]).
+         *
+         * Return dp[k][0] when the iteration is complete.
+         *
+         * @param events int array with event details start ady, end day and value of the event
+         * @param k maximum number of events you can attend
+         * @return maximum sum of values that you can receive by attending events
+         */
+        public int maxValuePartTwo(int[][] events, int k) {
+            int n = events.length;
+            int[][] dp = new int[k + 1][n + 1];
+            Arrays.sort(events, (a, b) -> a[0] - b[0]);
+
+            for (int curIndex = n - 1; curIndex >= 0; --curIndex) {
+                for (int count = 1; count <= k; count++) {
+                    int nextIndex = bisectRightDFS(events, events[curIndex][1]);
+                    dp[count][curIndex] = Math.max(dp[count][curIndex + 1], events[curIndex][2] + dp[count - 1][nextIndex]);
+                }
+            }
+            return dp[k][0];
+        }
+
+        private int bisectRightDFS(int[][] events, int target) {
+            int left = 0, right = events.length;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (events[mid][0] <= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+
+        public static void main(String[] args) {
+
+        }
+
+
+    }
 }
 
 /**
